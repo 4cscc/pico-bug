3cd811fd4ac041b65d0be9e43c6cc1cbf7d4da7d
Keegan Evans <keegan.evans@gmail.com>
2023-11-15 14:28:39 -0700

    FEAT: atmospheric sensor(BME 280) working

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from sensor import Sensor
 from bme280 import BME280
-from machine import WDT
 
-# sensor = Sensor(sensor_name="soil_moisture_demo_1")
-# sensor.register_analog_sensor_function("soil_moisture", 28)
-
-# I2C sensor example
 sensor = Sensor(sensor_name="atmospheric",
                 topic='sensor_data/atmospheric',
-                I2CSensor=True,
-                wdt=WDT(timeout=7000))
+                reporting_interval_sec=2,
+                I2CSensor=True)
 
 environ_sensor = BME280(i2c=sensor.i2c_bus)
 sensor.register_i2c_sensor_function('temperature',
@@ -19,5 +14,5 @@ sensor.register_i2c_sensor_function('humidity',
 sensor.register_i2c_sensor_function('pressure',
                                     environ_sensor.read_pressure)
 
-if __name__ == "__main__":
-    sensor.run()
+
+sensor.run()
--- mains_and_manifests/atmospheric_manifest.py
+++
@@ -1,2 +0,0 @@
-require('mqtt.simple')
-package(./pico_bug)
